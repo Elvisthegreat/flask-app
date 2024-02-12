b72139f8a71e9a27f18c0b6dafbc73cb747c8131
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-02-12 17:22:37 +0000

    Added out index.html and about.html

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
-import os
-from flask import Flask
+import os # A portable way of interacting
+from flask import Flask, render_template # The render_template function is used to render HTML templates
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def imdex():
-    return "Hello World"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(
