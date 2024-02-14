539bde563e2401480916ab1986269d81d612888e
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-02-14 13:44:57 +0000

    run.py

--- run.py
+++ run.py
@@ -11,17 +11,17 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About", list_of_numbers=[1,2,3]) # Expression to display about.html on page from backend to frontend
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact") # Expression to display contact.html on page from backend to frontend
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers") # Expression to display careers.html on page from backend to frontend
 
 if __name__ == "__main__":
     app.run(
